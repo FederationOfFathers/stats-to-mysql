8d2d5973096ad0568f2df635449f33511204e057
apokalyptik <apokalyptik@apokalyptik.com>
2016-09-02 04:02:56 +0000

    locking

--- user-lookup.go
+++ user-lookup.go
@@ -1,33 +1,44 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 
 var userToID = userLookup{}
+var userLookupLock sync.RWMutex
 
 type userLookup map[string]string
 
 func (u userLookup) find(userID string) string {
+	userLookupLock.RLock()
 	memberID, ok := u[userID]
-	if !ok {
-		_, err := userAdd.Exec(userID)
-		if err != nil {
-			log.Println("Error adding user", userID)
-		}
-		rows, err := userGet.Query(userID)
-		if err != nil {
-			log.Fatal("Error looking up user", userID)
-		} else {
-			for rows.Next() {
-				err := rows.Scan(&memberID)
-				if err != nil {
-					log.Println("Error scanning user lookup result into memberID", err)
-				} else {
-					userToID[userID] = memberID
-					log.Println("found ID for", userID, "=", memberID)
-				}
+	userLookupLock.RUnlock()
+	if ok {
+		return memberID
+	}
+
+	_, err := userAdd.Exec(userID)
+	if err != nil {
+		log.Println("Error adding user", userID)
+	}
+
+	rows, err := userGet.Query(userID)
+	if err != nil {
+		log.Fatal("Error looking up user", userID)
+	} else {
+		for rows.Next() {
+			err := rows.Scan(&memberID)
+			if err != nil {
+				log.Println("Error scanning user lookup result into memberID", err)
+			} else {
+				userLookupLock.Lock()
+				userToID[userID] = memberID
+				userLookupLock.Unlock()
+				log.Println("found ID for", userID, "=", memberID)
 			}
 		}
-		rows.Close()
 	}
+	rows.Close()
 	return memberID
 }
